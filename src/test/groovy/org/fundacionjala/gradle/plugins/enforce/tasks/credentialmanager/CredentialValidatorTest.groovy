0545aab8c729f900f03ee4097c31ad621580062b
Dan Canqui <dan.canqui@jalasoft.com>
2016-01-06 14:07:00 -0400

    Add negative case for validate credentials.

---
+++ src/test/groovy/org/fundacionjala/gradle/plugins/enforce/tasks/credentialmanager/CredentialValidatorTest.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Fundacion Jala. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package org.fundacionjala.gradle.plugins.enforce.tasks.credentialmanager
+
+import org.fundacionjala.gradle.plugins.enforce.wsc.Connector
+import org.fundacionjala.gradle.plugins.enforce.wsc.Credential
+import org.fundacionjala.gradle.plugins.enforce.wsc.Session
+import spock.lang.Shared
+import spock.lang.Specification
+
+class CredentialValidatorTest extends Specification {
+    @Shared
+    Connector connector
+    @Shared
+    Session session
+
+    def setup() {
+        connector = Mock(Connector)
+        session = Mock(Session)
+        connector.login(_) >> { Credential credential ->
+            if (credential.id == "default" &&
+                    credential.username == "username@email.com" &&
+                    credential.password == "0bdzKV14J7bs2h1yw01eMQ==" &&
+                    credential.token == "xthkbHWGppQTkrqFhHzpzw==") {
+                return session
+            }
+            throw new Exception()
+        }
+    }
+
+    def "Test should throw exception if credential is null"() {
+        when:
+        CredentialValidator.validate(null)
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "Test should return 'false' if credential is not active"() {
+        given:
+        Credential credential = new Credential()
+        credential.id = "invalid"
+        credential.username = "invaliduser@email.com"
+        credential.token = "h6Sxot/neQychayvgq+tkg=="
+        credential.password = "q30XOAOUVuyD7+zVrIZypQ=="
+        when:
+        boolean validated = CredentialValidator.validate(credential)
+        then:
+        validated == false
+    }
+}
